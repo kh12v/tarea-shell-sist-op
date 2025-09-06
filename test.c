64efd011c9b64447e8f41f36848123f5d0579255
kh12v <154763602+kh12v@users.noreply.github.com>
2025-09-06 12:26:20 -0400

    Primera shell funcional

--- test.c
+++ test.c
@@ -1,7 +1,5 @@
 #include <string.h>
 #include <stdio.h>
-#include <unistd.h>
-#include <sys/wait.h>
 
 #define MAX_LINE_LENGTH 100
 #define true 1
@@ -12,8 +10,6 @@ bool readLine(char* line) {
     printf("Ingrese un comando:\n");
     
     if (fgets(line, MAX_LINE_LENGTH, stdin) != NULL) {
-        // Elimina el carácter de nueva línea ('\n') del final
-        line[strcspn(line, "\n")] = 0;
         return true;
     } else {
         printf("Error al leer la entrada.");
@@ -24,35 +20,29 @@ bool readLine(char* line) {
 void readCommand(char* line, char** program, char* args[]) {
     char* word = strtok (line, " ");
     *program = word;
+    printf("1 program: %s\n", *program);
     int n = 0;
     while (word != NULL) {
+        printf ("%s/", word);
+        word = strtok (NULL, " ");
         args[n] = word;
         n++;
-        word = strtok (NULL, " ");
     }
     args[n] = NULL;
+    printf("2 program: %s\n", *program);
 }
 
 int main() {
     char line[MAX_LINE_LENGTH];
-    pid_t pid;
+    char* program;
+    char* args[MAX_LINE_LENGTH];
 
-    while (readLine(line)) {
-        if (strcmp(line, "exit") == 0) {
+    readLine(line);
+    readCommand(line, &program, args);
+
+    for (int i = 0; i < MAX_LINE_LENGTH; i++) {
+        if (args[i] == NULL) {
             break;
-        }
-        pid = fork();
-
-        if (pid == 0) {
-            char* program;
-            char* args[MAX_LINE_LENGTH];
-
-            readCommand(line, &program, args);
-            execvp(program, args);
-            printf("\n");
-            return 0;
-        } else {
-            wait(NULL);
         }
     }
 
