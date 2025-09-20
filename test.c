eb527ca7d4a632c77ab0005c9af9bd3c0baa031d
kh12v <154763602+kh12v@users.noreply.github.com>
2025-09-19 21:35:29 -0300

    pipes y redirect

--- test.c
+++ test.c
@@ -1,48 +1,89 @@
-#include <string.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/wait.h>
 
-#define MAX_LINE_LENGTH 100
-#define true 1
-#define false 0
-#define bool char
+char** parse_command(char *cmd) {
+    char** argv = malloc(64 * sizeof(char*));
+    char* token;
+    int i = 0;
 
-bool readLine(char* line) {
-    printf("Ingrese un comando:\n");
-    
-    if (fgets(line, MAX_LINE_LENGTH, stdin) != NULL) {
-        return true;
-    } else {
-        printf("Error al leer la entrada.");
+    token = strtok(cmd, " \t\n");
+    while (token != NULL) {
+        argv[i++] = token;
+        token = strtok(NULL, " \t\n");
     }
-    return false;
+    argv[i] = NULL;
+    return argv;
 }
 
-void readCommand(char* line, char** program, char* args[]) {
-    char* word = strtok (line, " ");
-    *program = word;
-    printf("1 program: %s\n", *program);
-    int n = 0;
-    while (word != NULL) {
-        printf ("%s/", word);
-        word = strtok (NULL, " ");
-        args[n] = word;
-        n++;
-    }
-    args[n] = NULL;
-    printf("2 program: %s\n", *program);
-}
+// cat texto.txt | grep que | paste >> texto2.txt
 
 int main() {
-    char line[MAX_LINE_LENGTH];
-    char* program;
-    char* args[MAX_LINE_LENGTH];
+    char input[1024];
+    while (1) {
+        printf("Comando: ");
+        if (!fgets(input, sizeof(input), stdin)) break;
 
-    readLine(line);
-    readCommand(line, &program, args);
+        if (strncmp(input, "exit", 4) == 0) break;
 
-    for (int i = 0; i < MAX_LINE_LENGTH; i++) {
-        if (args[i] == NULL) {
-            break;
+        // Separar por pipes
+        char* commands[64];
+        int ncmds = 0;
+        char* token = strtok(input, "|");
+        while (token != NULL) {
+            commands[ncmds++] = token;
+            token = strtok(NULL, "|");
+        }
+
+        // Creating pipes
+        int pipes[2 * (ncmds - 1)];
+        for (int i = 0; i < ncmds - 1; i++) {
+            if (pipe(pipes + i*2) == -1) {
+                perror("pipe");
+                exit(1);
+            }
+        }
+
+        // Executing commands
+        for (int i = 0; i < ncmds; i++) {
+            pid_t pid = fork();
+            if (pid == 0) {  // Child
+                if (i > 0) {
+                    if (dup2(pipes[(i-1)*2], STDIN_FILENO) == -1) {
+                        perror("dup2 stdin");
+                        exit(1);
+                    }
+                }
+                if (i < ncmds - 1) {
+                    if (dup2(pipes[i*2 + 1], STDOUT_FILENO) == -1) {
+                        perror("dup2 stdout");
+                        exit(1);
+                    }
+                }
+
+                // Cloising pipes
+                for (int j = 0; j < 2*(ncmds-1); j++) {
+                    close(pipes[j]);
+                }
+
+                // Executing
+                char **argv = parse_command(commands[i]);
+                execvp(argv[0], argv);
+                perror("execvp");
+                exit(1);
+            }
+        }
+
+        // Closing pipes
+        for (int i = 0; i < 2*(ncmds-1); i++) {
+            close(pipes[i]);
+        }
+
+        // Waiting children
+        for (int i = 0; i < ncmds; i++) {
+            wait(NULL);
         }
     }
 
