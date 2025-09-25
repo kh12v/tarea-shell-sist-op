da03b94d7b008345000d4cf07186671fe411eded
Bindhza <nexxhurclan@gmail.com>
2025-09-25 02:10:08 -0300

    Implementacion completa del comando miprof(falta probar)

--- main.c
+++ main.c
@@ -6,6 +6,7 @@
 #include <sys/wait.h>
 #include <signal.h>
 #include <sys/types.h>
+#include <sys/resource.h>
 
 
 char** parse_command(char *cmd) {
@@ -21,6 +22,14 @@ char** parse_command(char *cmd) {
     argv[i] = NULL;
     return argv;
 }
+
+void print_mem_peak(FILE *destino) {
+    struct rusage uso;
+    if (getrusage(RUSAGE_CHILDREN, &uso) == 0) {
+        fprintf(destino, "Peak memoria residente: %ld KB\n", uso.ru_maxrss);
+    }
+}
+
 
 // cat texto.txt | grep que | paste >> texto2.txt
 
@@ -108,28 +117,27 @@ int main() {
                         }
                         with_time[i] = NULL;
 
+                        //hijo ejecuta el comando y el padre mide memoria
+                        pid_t cpid = fork();
+                        if (cpid == 0) {
+                            if (outfile) {
+                                int fd;
+                                if (redirect_append)
+                                    fd = open(outfile, O_WRONLY | O_CREAT | O_APPEND, 0644);
+                                else
+                                    fd = open(outfile, O_WRONLY | O_CREAT | O_TRUNC, 0644);
 
-                        if (outfile) {
-                            int fd;
-                            if (redirect_append)
-                                fd = open(outfile, O_WRONLY | O_CREAT | O_APPEND, 0644);
-                            else
-                                fd = open(outfile, O_WRONLY | O_CREAT | O_TRUNC, 0644);
-
-                            if (fd < 0) {
-                                perror("open");
-                                exit(1);
+                                if (fd < 0) { perror("open"); exit(1); }
+                                if (dup2(fd, STDOUT_FILENO) == -1) { perror("dup2 redir"); exit(1); }
+                                close(fd);
                             }
-                            if (dup2(fd, STDOUT_FILENO) == -1) {
-                                perror("dup2 redir");
-                                exit(1);
-                            }
-                            close(fd);
+                            execvp(with_time[0], with_time);
+                            perror("execvp");
+                            exit(1);
+                        } else {
+                            waitpid(cpid, NULL, 0);
+                            print_mem_peak(stdout);
                         }
-
-                        execvp(with_time[0], with_time);
-                        perror("execvp");
-                        exit(1);
 
                     }
                     else if (argv[1] && strcmp(argv[1], "ejecsave") == 0){
@@ -138,7 +146,7 @@ int main() {
                         int redirect_append = 0;
                         char* outfile = NULL;
                         FILE *archivo; 
-                        char nombre[] = argv[2];
+                        char *nombre[] = argv[2];
                         archivo = fopen(nombre, "a");
 
                         for (int k = 3; argv[k]; k++) {
@@ -164,34 +172,35 @@ int main() {
                         }
                         with_time[i] = NULL;
 
-                        //almacena time
-                        int fd_archivo = fileno(archivo);
-                        if (dup2(fd_archivo, STDERR_FILENO) == -1) {
-                            perror("dup2 stderr");
+                        //lo mismo que el anterior
+                        pid_t cpid = fork();
+                        if (cpid == 0) {
+                            //almacena time
+                            int fd_archivo = fileno(archivo);
+                            if (dup2(fd_archivo, STDERR_FILENO) == -1) {
+                                perror("dup2 stderr");
+                                exit(1);
+                            }
+                            if (outfile) {
+                                int fd;
+                                if (redirect_append)
+                                    fd = open(outfile, O_WRONLY | O_CREAT | O_APPEND, 0644);
+                                else
+                                    fd = open(outfile, O_WRONLY | O_CREAT | O_TRUNC, 0644);
+
+                                if (fd < 0) { perror("open"); exit(1); }
+                                if (dup2(fd, STDOUT_FILENO) == -1) { perror("dup2 redir"); exit(1); }
+                                close(fd);
+                            }
+                            execvp(with_time[0], with_time);
+                            perror("execvp");
                             exit(1);
+                        } else {
+                            waitpid(cpid, NULL, 0);
+                            // Peak al archivo
+                            print_mem_peak(archivo);
+                            fclose(archivo);
                         }
-
-                        if (outfile) {
-                            int fd;
-                            if (redirect_append)
-                                fd = open(outfile, O_WRONLY | O_CREAT | O_APPEND, 0644);
-                            else
-                                fd = open(outfile, O_WRONLY | O_CREAT | O_TRUNC, 0644);
-
-                            if (fd < 0) {
-                                perror("open");
-                                exit(1);
-                            }
-                            if (dup2(fd, STDOUT_FILENO) == -1) {
-                                perror("dup2 redir");
-                                exit(1);
-                            }
-                            close(fd);
-                        }
-
-                        execvp(with_time[0], with_time);
-                        perror("execvp");
-                        exit(1);
 
                     }
                     else if (argv[1] && strcmp(argv[1], "ejecutar") == 0){
@@ -262,6 +271,10 @@ int main() {
                             if (waited >= timemaximo) {
                                 printf("Tiempo maximo alcanzado\n");
                                 kill(pid, SIGKILL);
+                            }
+                            //si no se mata el proseso muestra info a pantalla
+                            else{
+                                print_mem_peak(stdout);
                             }
                         } else {
                             perror("fork");
